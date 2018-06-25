efd53739078e9269456f810a7627e3450439c1e3
Pu <nomipu2015@gmail.com>
2018-06-26 01:49:53 +0700

    Revert "Revert "product by Cat page""
    This reverts commit 4a36e94f4f14351bac713e6675d2688d5879b5cb.

--- Web33Nhom8-master/Web_CuoiKy/controllers/productController.js
+++ Web33Nhom8-master/Web_CuoiKy/controllers/productController.js
@@ -27,4 +27,59 @@ router.get('/detail/:proId', (req, res) => {
 });
 
 
+
+
+
+
+
+
+
+
+
+router.get('/byCat/:catId', (req, res) => {
+    var catId = req.params.catId;
+
+    var page = req.query.page;
+    if (!page) page = 1;
+    if (page < 1) page = 1;
+
+    var offset = (page - 1) * config.PRODUCTS_PER_PAGE;
+
+    var p1 = productRepo.loadPageByCat(catId, offset);
+    var p2 = productRepo.countByCat(catId);
+    Promise.all([p1, p2]).then(([rows, count_rows]) => {
+        var total = count_rows[0].total;
+        var nPages = total / config.PRODUCTS_PER_PAGE;
+        if (total % config.PRODUCTS_PER_PAGE > 0)
+            nPages++;
+
+        var numbers = [];
+        for (i = 1; i <= nPages; i++) {
+            numbers.push({
+                value: i,
+                isCurrentPage: i === +page
+            });
+        }
+
+        var vm = {
+            nPages,
+            products: rows,
+            noProducts: rows.length === 0,
+            page_numbers: numbers
+        };
+        res.render('product/byCat', vm);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
