ea5051332d11b4f31b2ea1e1bc2318bd687bebe7
Pu <nomipu2015@gmail.com>
2018-06-26 01:01:28 +0700

    product by Cat page

--- Web33Nhom8-master/Web_CuoiKy/repos/productRepo.js
+++ Web33Nhom8-master/Web_CuoiKy/repos/productRepo.js
@@ -29,31 +29,15 @@ exports.load4ByProducer = (producerId, offset) => {
 }
 
 
-
-
-
-
-
-
-
 exports.loadAllByCat = catId => {
 	var sql = `select * from products where CatID = ${catId}`;
 	return db.load(sql);
 }
 
-
-
-
-
-
-
-
-
 exports.loadPageByCat = (catId, offset) => {
 	var sql = `select * from products where CatID = ${catId} limit ${config.PRODUCTS_PER_PAGE} offset ${offset}`;
 	return db.load(sql);
 }
-
 exports.countByCat = catId => {
 	var sql = `select count(*) as total from products where CatID = ${catId}`;
 	return db.load(sql);
